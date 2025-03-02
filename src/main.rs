b986c22808ad619105e6ea5accdc858c451487e1
Jörg Zimmermann <ekneg54@pm.me>
2025-03-02 11:53:48 +0100

    add simple cli with clap

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,51 @@
-use loggen_rs::{read_yaml_file, Config};
+use std::path::PathBuf;
 
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name="loggen", version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+
+    /// Path to log config directory
+    #[arg(short, long, value_name = "DIR")]
+    config: Option<PathBuf>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// send logs to an http endpoint
+    Http {
+        /// base url of the http endpoint. Default is http://localhost:9000
+        #[arg(short, long)]
+        url: String,
+    },
+
+    /// send logs to a kafka topic
+    Kafka {
+        /// kafka config as a mapping of key value pairs
+        /// e.g. "{bootstrap.servers: localhost:9092, topic: loggen}"
+        /// Default is "{bootstrap.servers: localhost:9092, topic: producer}"
+        #[arg(short, long)]
+        kafkaconfig: String,
+    },
+
+}
 
 
 fn main() {
-    let path = "test_config.yaml";
-    let result = read_yaml_file::<Config, &str>(path);
-    match result {
-        Ok(config) => {
-            println!("{:?}", config);
+    let cli = Cli::parse();
+
+    match cli.command {
+        Some(Commands::Http { url }) => {
+            println!("Sending logs to http endpoint");
         }
-        Err(e) => {
-            eprintln!("Error: {}", e);
+        Some(Commands::Kafka { kafkaconfig }) => {
+            println!("Sending logs to kafka topic");
+        }
+        None => {
+            println!("No command provided");
         }
     }
 }
